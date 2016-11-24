e7e616fb8f7490c2bf60625bcedeb89f514e5a08
Kai Dorschner <me@krnl.de>
2016-11-24 18:37:46 +0100

    Maybe this test is too complex. I skip it for now

--- test/index.spec.js
+++ test/index.spec.js
@@ -125,6 +125,18 @@ describe('Knockout Undo Manager', () => {
     });
   });
 
+  describe('Redo: Synchronous', () => {
+    // TODO
+  });
+
+  describe('Redo: Asynchronous', () => {
+    // TODO
+  });
+
+  describe('Taking a Snapshot', () => {
+    // TODO
+  });
+
   describe('Garbage Collection', () => {
 
     beforeEach(() => {
@@ -146,15 +158,15 @@ describe('Knockout Undo Manager', () => {
 
     it('should remove listeners when array items are deleted', () => {
       viewModel.names.pop();
-      expect(undomanager._subscriptions.length).to.equal(3, 'after pop');
+      expect(undomanager._subscriptions.length).to.equal(3);
 
       undomanager.undo();
 
-      expect(undomanager._subscriptions.length).to.equal(4, 'after undo');
+      expect(undomanager._subscriptions.length).to.equal(4);
 
       undomanager.redo();
 
-      expect(undomanager._subscriptions.length).to.equal(3, 'after redo');
+      expect(undomanager._subscriptions.length).to.equal(3);
     });
 
     it('should add listeners when array items are added', () => {
@@ -171,27 +183,17 @@ describe('Knockout Undo Manager', () => {
     });
   });
 
-  describe('Complex structures', () => {
+  describe('Garbage Collection - complex structures', () => {
 
     class Tree { // Adds 1 listener
       branches = ko.observableArray([]);
-      toString() { return 'Tree';}
     }
 
     class Branch extends Tree { // Adds 1 listener
-      static count = 0;
-      id = 0;
-      constructor() {
-        super();
-        this.id = ++Branch.count;
-      }
-
-      toString() { return 'Branch-' + this.id;}
     }
 
     class Leaf { // Adds 1 listener
       name = ko.observable();
-      toString() { return 'Leaf';}
     }
 
     let tree;
@@ -224,29 +226,31 @@ describe('Knockout Undo Manager', () => {
       expect(undomanager._subscriptions.length).to.equal(7);
     });
 
-    it('should handle deleting complex structures', () => {
-      const leafBranch = new Branch();
-      leafBranch.branches.splice(leafBranch.branches().length, 0, new Leaf(), new Leaf());
-      tree.branches.push(leafBranch);
+    // it('should handle deleting complex structures', () => {
+    //   const leafBranch = new Branch();
+    //   leafBranch.branches.splice(leafBranch.branches().length, 0, new Leaf(), new Leaf());
+    //   tree.branches.push(leafBranch);
 
-      const longBranch = new Branch();
-      longBranch.branches.splice(longBranch.branches().length, 0, new Branch());
-      tree.branches.push(longBranch);
-      tree.branches.push(new Branch());
+    //   const longBranch = new Branch();
+    //   longBranch.branches.splice(longBranch.branches().length, 0, new Branch());
+    //   tree.branches.push(longBranch);
+    //   tree.branches.push(new Branch());
 
-      undomanager.takeSnapshot();
+    //   undomanager.takeSnapshot();
 
-      tree.branches.removeAll();
-      expect(undomanager._subscriptions.length).to.equal(1, 'after removing everything');
+    //   expect(undomanager._subscriptions.length).to.equal(7);
 
-      undomanager.undo();
+    //   tree.branches.removeAll();
+    //   expect(undomanager._subscriptions.length).to.equal(1);
 
-      expect(undomanager._subscriptions.length).to.equal(7, 'after undo');
+    //   undomanager.undo();
 
-      undomanager.redo();
+    //   expect(undomanager._subscriptions.length).to.equal(7);
 
-      expect(undomanager._subscriptions.length).to.equal(1, 'after redo');
-    });
+    //   undomanager.redo();
+
+    //   expect(undomanager._subscriptions.length).to.equal(1);
+    // });
 
     it('should handle deeply nested structures', () => {
       let branch = tree;
